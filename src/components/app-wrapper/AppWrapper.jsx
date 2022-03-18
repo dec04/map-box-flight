4c0b980bfaf3d8cd902521ca02c10a80a524a1b4
Dec04 <dmedia.decoy@gmail.com>
2022-03-18 17:57:18 +0700

    Add tailwind plugins;
    Signed-off-by: Vitaliy Sokolov <Vitaliy.O.Sokolov@rt.ru>

--- src/components/app-wrapper/AppWrapper.jsx
+++ src/components/app-wrapper/AppWrapper.jsx
@@ -6,6 +6,18 @@ export default class AppWrapper extends React.Component {
             <h1>💖 Hello World!</h1>
             <p>Welcome to your Electron application.</p>
             <button className='bg-indigo-500'>Tailwind</button>
+            <article className="prose prose-stone">
+                <h1>Prose test</h1>
+                <p>
+                    For years parents have espoused the health benefits of eating garlic bread with cheese to their
+                    children, with the food earning such an iconic status in our culture that kids will often dress
+                    up as warm, cheesy loaf for Halloween.
+                </p>
+                <p>
+                    But a recent study shows that the celebrated appetizer may be linked to a series of rabies cases
+                    springing up around the country.
+                </p>
+            </article>
         </div>;
     }
 }
