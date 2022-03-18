bd35b580ce3e784befc9f6b5cefb3aa751973aea
Dec04 <dmedia.decoy@gmail.com>
2022-03-18 17:10:02 +0700

    Second
    Signed-off-by: Vitaliy Sokolov <Vitaliy.O.Sokolov@rt.ru>

---
+++ src/components/app-wrapper/AppWrapper.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class AppWrapper extends React.Component {
+    render() {
+        return <div>
+            <h1>💖 Hello World!</h1>
+            <p>Welcome to your Electron application.</p>
+        </div>;
+    }
+}
+
---
+++ src/react-connect.js
@@ -0,0 +1,5 @@
+import React from "react";
+import * as ReactDOM from "react-dom";
+import AppWrapper from "./components/app-wrapper/AppWrapper.jsx";
+
+ReactDOM.render(<AppWrapper/>, document.getElementById("root-app"));
